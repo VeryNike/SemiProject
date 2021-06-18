caefe2a1d19eb579d65eda05adaa583abef43c19
lee <rudehd514@naver.com>
2021-06-18 18:00:45 +0900

    board update

--- src/board/model/service/BoardService.java
+++ src/board/model/service/BoardService.java
@@ -1,5 +1,38 @@
 package board.model.service;
 
+import static common.JDBCTemplate.getConnection;
+import static common.JDBCTemplate.close;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import board.model.dao.BoardDAO;
+import board.model.vo.Board;
+import board.model.vo.PageInfo;
+
+
 public class BoardService {
+
+	public int getListCount() {
+		
+		Connection conn = getConnection();
+		
+		int result = new BoardDAO().getListCount(conn);
+		
+		close(conn);
+		
+		return result;
+	}
+
+	public ArrayList<Board> selectList(PageInfo pi) {
+		Connection conn = getConnection();
+		
+		ArrayList<Board> list = new BoardDAO().slectList(conn,pi);
+		
+		close(conn);
+		
+		return list;
+	
+	}
 
 }
