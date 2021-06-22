4002be52d09e85f52df9836f3bb367492000c89f
leekyongdong <84305792+leekyongdong@users.noreply.github.com>
2021-06-22 17:47:23 +0900

    Add files via upload

---
+++ src/board/controller/BoardListServlet.java
@@ -0,0 +1,88 @@
+package board.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import board.model.service.BoardService;
+import board.model.vo.Board;
+import board.model.vo.PageInfo;
+
+/**
+ * Servlet implementation class BoardListServlet
+ */
+@WebServlet("/list.me")
+public class BoardListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BoardService service = new BoardService();
+		
+		int listCount = service.getListCount();
+		int currentPage;
+		int pageLimit = 10;
+		int maxPage;
+		int startPage;
+		int endPage;
+		int boardLimit = 10;
+		
+		currentPage = 1;
+		
+		if(request.getParameter("currentPage")!= null) {
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));
+		}
+		
+		maxPage = (int)Math.ceil((double)listCount/pageLimit);
+		
+		startPage = ((currentPage-1)/pageLimit)*pageLimit +1 ;
+		
+		endPage = pageLimit + startPage - 1;
+		
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
+		
+		PageInfo pi = new PageInfo(currentPage,listCount,pageLimit,maxPage,startPage,endPage,boardLimit);
+		
+		ArrayList<Board> list = service.selectList(pi);
+		
+		String page = null;
+		if(list != null) {
+			page = "WEB-INF/view/board/BoardList.jsp";
+			request.setAttribute("list", list);
+			request.setAttribute("pi", pi);
+		}else {
+			page = "WEB-INF/common/erroerPage.jsp";
+			request.setAttribute("msg","게시판 조회 실패.");
+			
+		}
+		
+		RequestDispatcher view = request.getRequestDispatcher(page);
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
