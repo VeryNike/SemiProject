d973b4c2af6666c68f28dedafa1cb827f4544485
HyeYeon-2 <84279882+HyeYeon-2@users.noreply.github.com>
2021-06-22 20:04:37 +0900

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -777,4 +777,37 @@ START WITH 1 INCREMENT BY 1
 MAXVALUE 99999
 NOCYCLE;
 
+INSERT INTO PT VALUES(1, '전신', '버피 (Burpee)');
+INSERT INTO PT VALUES(2, '전신', '마운틴 클라이버(Moutain climber)');
+INSERT INTO PT VALUES(3, '전신', '플랭크 (Plank)');
+INSERT INTO PT VALUES(4, '전신', '푸쉬업(Push-up)');
+INSERT INTO PT VALUES(5, '전신', '스쿼드 점프(Squat jump)');
+INSERT INTO PT VALUES(6, '전신', '런지 (Lunge)');
+INSERT INTO PT VALUES(7, '하체', '스쿼트 (squat)');
+INSERT INTO PT VALUES(8, '하체', '레그프레스 (Leg press)');
+INSERT INTO PT VALUES(9, '하체', '레그 익스텐션 (Leg extension)');
+INSERT INTO PT VALUES(10, '하체', '레그 컬(Leg curl)');
+INSERT INTO PT VALUES(11, '하체', '데드리프트(Deadlift)');
+INSERT INTO PT VALUES(12, '팔', '덤벨 컬 (Dumbbell ? curl)');
+INSERT INTO PT VALUES(13, '팔', '바벨 컬 (Barbell ? curl)');
+INSERT INTO PT VALUES(14, '팔', '트라이 셉스 푸시다운 (Triceps Pushdown)');
+INSERT INTO PT VALUES(15, '팔', '트라이 셉스 익스텐션 (Triceps Extension)');
+INSERT INTO PT VALUES(16, '팔', '덤벨 시티드 오버헤드 프레스(Dumbbell seated overhead press)');
+INSERT INTO PT VALUES(17, '가슴', '벤치 프레스 (Bench press)');
+INSERT INTO PT VALUES(18, '가슴', '덤벨 플라이 (Dumbbell Fly)');
+INSERT INTO PT VALUES(19, '가슴', '푸쉬 업 (Push-up)');
+INSERT INTO PT VALUES(20, '가슴', '케이블 크로스 오버 (Cable Cross over)');
+INSERT INTO PT VALUES(21, '가슴', '딥스 (Dips)');
+INSERT INTO PT VALUES(22, '복근', '시티드 니업 (Seated Knee Up)');
+INSERT INTO PT VALUES(23, '복근', '브이 업 (V-up)');
+INSERT INTO PT VALUES(24, '복근', '러시안 트위스트 (Russian-twist)');
+INSERT INTO PT VALUES(25, '복근', '레그 레이즈 (Leg ? raise)');
+INSERT INTO PT VALUES(26, '복근', '크런치 (Crunch)');
+INSERT INTO PT VALUES(27, '복근', '푸쉬업 홀드 (Push-up hold)');
+INSERT INTO PT VALUES(28, '등', '벤트 오버 바벨로우(Bent-over Barbell Row)');
+INSERT INTO PT VALUES(29, '등', '렛풀다운 (Lat pull down)');
+INSERT INTO PT VALUES(30, '등', '풀업 (Pull-up)');
+INSERT INTO PT VALUES(31, '등', '시티드 케이블 로우 (Seated cable row)');
+INSERT INTO PT VALUES(32, '등', '티바 로우 (T-bar Row)');
+
 commit;
