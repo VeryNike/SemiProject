07b7f712b0b96d956cd315ceca38479c1615c625
junghwan9276 <84328342+junghwan9276@users.noreply.github.com>
2021-06-25 17:32:38 +0900

    Update Common.java

--- src/common/Common.java
+++ src/common/Common.java
@@ -3,7 +3,19 @@ package common;
 public class Common {
 	//public static final String WEB_URL = "WEB-INF/view";
 	public static final String ERR_URL = "WEB-INF/view/common/errorPage.jsp";
+	
+	
+	/////////////////////////2021-06-25 + ///////////////////////////////////
+	public static String nvl(String str) {
+		return str == null ? "" : str;
+	}
 
-
+	public static String nvl(String str, String repl) {
+		if (nvl(str).trim().equals("")) {
+			return repl;
+		}
+		return str;
+	}
+     
 
 }
