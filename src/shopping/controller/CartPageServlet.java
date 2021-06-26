4ba940d1a8f43a935ece0163033fd17ec3c2c40a
jnghwn <84328342+jnghwn@users.noreply.github.com>
2021-06-26 14:54:09 +0900

    Update CartPageServlet.java

--- src/shopping/controller/CartPageServlet.java
+++ src/shopping/controller/CartPageServlet.java
@@ -7,38 +7,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class CartPageServlet
- */
+import common.Common;
+import shopping.model.service.ItemService;
+import shopping.model.vo.Item;
+
 @WebServlet("/cart.me")
 public class CartPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CartPageServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		request.getRequestDispatcher("WEB-INF/view/shopping/cart.jsp").forward(request, response);
+		String itemCode = Common.nvl(request.getParameter("itemCode"));
+		Item item = new ItemService().selectProductInfo(itemCode);
+		String page = null;
 
-			
+		if (item != null) {
+			page = "WEB-INF/view/shopping/cart.jsp";
+			request.setAttribute("item", item);
+		} else {
+			page = "WEB-INF/view/common/errorPage.jsp";
+			request.setAttribute("msg", "상품 리스트 조회에 실패하였습니다.");
 		}
-	
+		request.getRequestDispatcher(page).forward(request, response);
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
