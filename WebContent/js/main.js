3c118a47a3bd3e9b49e58fb1c18704cdfbc9f27c
jnghwn <84328342+jnghwn@users.noreply.github.com>
2021-06-29 02:36:24 +0900

    vegas plugin add
    vegas plugin add

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,4 +1,20 @@
 (function($) {
+	////////vegas//////////////
+	$(document).ready(function(){
+		$("body").vegas({
+			delay:7500,
+			slides:[
+				{src:"img/bg1.jpg"},
+				{src:"img/bg3.jpg"},
+				{src:"img/checkbg.jpg"},
+				{src:"img/bg2.jpg"}
+			],
+			overlay:"overlays/01.png",
+			animation:"kenburnsUpLeft"
+		
+		})
+	});
+	
 	"use strict";
 
 	// Initiate the wowjs
