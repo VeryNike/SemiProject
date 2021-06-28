be902c791ca6a6e25f7eb3e782f42fa6831db102
KangSeungMin <o199390@gmail.com>
2021-06-28 15:29:48 +0900

    게시판 글쓰기 Servlet 업로드
    게시판 글쓰기 Servlet 업로드

---
+++ src/board/controller/BoardInsertServlet.java
@@ -0,0 +1,74 @@
+package board.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import board.model.service.BoardService;
+import board.model.vo.Board;
+import user.model.vo.User;
+
+/**
+ * Servlet implementation class BoardInsertServlet
+ */
+@WebServlet("/insert.bo")
+public class BoardInsertServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BoardInsertServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String category = request.getParameter("category");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		
+		HttpSession session = request.getSession();
+		
+		User loginUser = (User)session.getAttribute("loginUser");
+		String writer = loginUser.getUserId();
+		System.out.println("3" + writer);
+		
+		Board b = new Board();
+		b.setCategory(category);
+		b.setPsTitle(title);
+		b.setPsContent(content);
+		b.setPsWriter(writer);
+		b.setbType(1);
+		
+		int result = new BoardService().insertBoard(b);
+		
+		if(result > 0) {
+			response.sendRedirect("boardList.me?currentPage=1");
+		} else {
+			request.setAttribute("msg", "게시글 작성에 실패하였습니다.");
+			request.getRequestDispatcher("WEB-INF/view/common/errorPage.jsp").forward(request, response);
+		}
+	
+			
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
