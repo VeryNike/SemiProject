5e2f0e05e2e66dda773eed4d1b37f55f008cf8ed
jnghwn <84328342+jnghwn@users.noreply.github.com>
2021-06-29 21:08:35 +0900

    src/shopping/controller/OrderFormPageServlet.java
    cart -> checkout.jsp 로 넘기기위한 servlet수정

--- src/shopping/controller/OrderFormPageServlet.java
+++ src/shopping/controller/OrderFormPageServlet.java
@@ -1,11 +1,17 @@
 package shopping.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import common.Common;
+import shopping.model.service.ItemService;
+import shopping.model.vo.Item;
 
 @WebServlet("/checkout.me")
 public class OrderFormPageServlet extends HttpServlet {
@@ -19,6 +25,18 @@ public class OrderFormPageServlet extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String itemCode = Common.nvl(request.getParameter("item"));
+		Item item = new ItemService().selectCheckoutForm(itemCode);
+		ArrayList<Item> list = new ArrayList<Item>();
+		
+		
+		
+		if (item.getItemCode() != null) {
+			request.setAttribute("item", item);
+			request.setAttribute("list", list);
+		}
+
 		request.getRequestDispatcher("WEB-INF/view/shopping/checkout.jsp").forward(request, response);
 	}
 
