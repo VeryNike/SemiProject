f762584032ae8d9ac6fd27b4d277a1cd04ec8c2e
KangSeungMin <o199390@gmail.com>
2021-06-29 10:15:13 +0900

    BoardService.java
    BoardService.java 완료

--- src/board/model/service/BoardService.java
+++ src/board/model/service/BoardService.java
@@ -2,6 +2,8 @@ package board.model.service;
 
 import static common.JDBCTemplate.getConnection;
 import static common.JDBCTemplate.close;
+import static common.JDBCTemplate.commit;
+import static common.JDBCTemplate.rollback;
 
 import java.sql.Connection;
 import java.util.ArrayList;
@@ -24,15 +26,78 @@ public class BoardService {
 		return result;
 	}
 
-	public ArrayList<Board> selectList(PageInfo pi) {
+	public ArrayList<Board> selectList() {
 		Connection conn = getConnection();
 		
-		ArrayList<Board> list = new BoardDAO().slectList(conn,pi);
+		ArrayList<Board> list = new BoardDAO().selectList(conn);
 		
 		close(conn);
 		
 		return list;
 	
+	}
+
+	public int insertBoard(Board b) {
+		Connection conn = getConnection();
+		
+		int result = new BoardDAO().insertBoard(conn, b);
+		
+		if(result > 0) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		return result;
+	}
+
+	public Board selectBoard(int no) {
+		Connection conn = getConnection();
+		
+		BoardDAO bDAO = new BoardDAO();
+		
+		Board board = null;
+		
+		board = bDAO.selectBoard(conn,no);
+		
+		close(conn);
+		
+		return board;
+		
+	}
+
+	public int updateBoard(Board board) {
+		
+		Connection conn = getConnection();
+		
+		int result = new BoardDAO().updateBoard(conn, board);
+		
+		if(result > 0) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		
+		close(conn);
+			
+		return result;
+	}
+
+	public int deleteBoard(int no) {
+		Connection conn = getConnection();
+		
+		int result = new BoardDAO().deleteBoard(conn, no);
+		
+		if(result > 0) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+		
+		close(conn);
+		
+		return result;
 	}
 
 }
