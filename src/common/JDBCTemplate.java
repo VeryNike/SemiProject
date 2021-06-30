5e71c3441dba84cedb98d18f24d75aeef3502cbd
jnghwn <84328342+jnghwn@users.noreply.github.com>
2021-06-30 15:42:08 +0900

    PreparedStatement 추가한 구문 import 추가
    PreparedStatement 추가한 구문 import 추가

--- src/common/JDBCTemplate.java
+++ src/common/JDBCTemplate.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
