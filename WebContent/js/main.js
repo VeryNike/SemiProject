97b80cae76cd506c3c906bd2aefc42ae7458ce91
HyeYeon-2 <84279882+HyeYeon-2@users.noreply.github.com>
2021-06-30 20:35:18 +0900

    Update main.js

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,19 +1,4 @@
 (function($) {
-	////////vegas//////////////
-	$(document).ready(function(){
-		$("body").vegas({
-			delay:7500,
-			slides:[
-				{src:"img/bg1.jpg"},
-				{src:"img/bg3.jpg"},
-				{src:"img/checkbg.jpg"},
-				{src:"img/bg2.jpg"}
-			],
-			overlay:"overlays/01.png",
-			animation:"kenburnsUpLeft"
-		
-		})
-	});
 	
 	"use strict";
 
