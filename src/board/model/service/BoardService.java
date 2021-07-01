6af3a51a3055689746642b3fd99425bb9c4972fc
KangSeungMin <o199390@gmail.com>
2021-07-01 17:34:28 +0900

    BoardService.java
    댓글 부분 추가

--- src/board/model/service/BoardService.java
+++ src/board/model/service/BoardService.java
@@ -1,8 +1,8 @@
 package board.model.service;
 
-import static common.JDBCTemplate.getConnection;
 import static common.JDBCTemplate.close;
 import static common.JDBCTemplate.commit;
+import static common.JDBCTemplate.getConnection;
 import static common.JDBCTemplate.rollback;
 
 import java.sql.Connection;
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 import board.model.dao.BoardDAO;
 import board.model.vo.Board;
-import board.model.vo.PageInfo;
+import board.model.vo.Reply;
 
 
 public class BoardService {
@@ -99,5 +99,67 @@ public class BoardService {
 		
 		return result;
 	}
+	
+	public ArrayList<Reply> insertReply(Reply r) {
+		Connection conn = getConnection();
+		
+		BoardDAO dao = new BoardDAO();
+		
+		int result = dao.insertReply(conn,r);
+		
+		ArrayList<Reply> replys = null;
+	
+		if (result >0) {
+			commit(conn);
+			replys = dao.listReply(conn,r.getB_no());
+			
+		}else {
+			rollback(conn);
+		}
+		return replys;
+
+	}
+
+	public ArrayList<Reply> listReply(int no) {
+		Connection conn = getConnection();
+		ArrayList<Reply> replys = new BoardDAO().listReply(conn, no);
+		close(conn);
+		
+		return replys;
+	}
+
+	public ArrayList<Reply> updateReply(Reply r) {
+		Connection conn = getConnection();
+		BoardDAO dao = new BoardDAO();
+		int result = dao.updateReply(conn,r);
+		ArrayList<Reply> replys = null;
+		
+		if(result >0) {
+			commit(conn);
+			replys = dao.listReply(conn, r.getB_no());
+		}else {
+			rollback(conn);
+		}
+		close(conn);
+		return replys;
+	}
+
+	public int deleteReply(Reply r) {
+		Connection conn = getConnection();
+		
+		int result = new BoardDAO().deleteReply(conn, r);
+		System.out.println("result:" +result);
+		if(result>0) {
+			commit(conn);
+		}else {
+			rollback(conn);
+		}
+		System.out.println(result);
+				
+		return result;
+	}
+
+	
+
 
 }
