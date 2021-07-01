bb98886b847ee2ec7b73e89e51b18eadc5c945fe
jnghwn <84328342+jnghwn@users.noreply.github.com>
2021-07-01 18:59:59 +0900

    Update MyUpdateFormServlet.java

--- src/user/controller/MyUpdateFormServlet.java
+++ src/user/controller/MyUpdateFormServlet.java
@@ -9,24 +9,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import user.model.vo.User;
 
-/**
- * Servlet implementation class MyUpdateFormServelet
- */
+
 @WebServlet(urlPatterns="/MyUpdateForm.me", name="MyUpdateFormServelet")
-public class MyUpdateFormServelet extends HttpServlet {
+public class MyUpdateFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MyUpdateFormServelet() {
+ 
+    public MyUpdateFormServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		
@@ -48,9 +41,7 @@ public class MyUpdateFormServelet extends HttpServlet {
 		request.getRequestDispatcher("WEB-INF/view/user/myUpdate.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
