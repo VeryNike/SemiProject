fe451a76edd7a9d29b35f71af7d0601373f1898a
KangSeungMin <o199390@gmail.com>
2021-07-01 19:29:42 +0900

    BoardListServlet.java
    페이징 부분 삭제

--- src/board/controller/BoardListServlet.java
+++ src/board/controller/BoardListServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import board.model.service.BoardService;
 import board.model.vo.Board;
-import board.model.vo.PageInfo;
 
 /**
  * Servlet implementation class BoardListServlet
@@ -35,39 +34,12 @@ public class BoardListServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		BoardService service = new BoardService();
 		
-		int listCount = service.getListCount();
-		int currentPage;
-		int pageLimit = 10;
-		int maxPage;
-		int startPage;
-		int endPage;
-		int boardLimit = 10;
-		
-		currentPage = 1;
-		
-		if(request.getParameter("currentPage")!= null) {
-			currentPage = Integer.parseInt(request.getParameter("currentPage"));
-		}
-		
-		maxPage = (int)Math.ceil((double)listCount/pageLimit);
-		
-		startPage = ((currentPage-1)/pageLimit)*pageLimit +1 ;
-		
-		endPage = pageLimit + startPage - 1;
-		
-		if(endPage > maxPage) {
-			endPage = maxPage;
-		}
-		
-		PageInfo pi = new PageInfo(currentPage,listCount,pageLimit,maxPage,startPage,endPage,boardLimit);
-		
 		ArrayList<Board> list = service.selectList();
 		
 		String page = null;
 		if(list != null) {
 			page = "WEB-INF/view/board/boardList.jsp";
 			request.setAttribute("list", list);
-			request.setAttribute("pi", pi);
 		}else {
 			page = "WEB-INF/common/erroerPage.jsp";
 			request.setAttribute("msg","게시판 조회 실패.");
